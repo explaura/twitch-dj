917a77515945504a190cdd808b9e1dfa095fa3c0
Laura <laurakbrandon@gmail.com>
2020-05-19 19:29:31 +1000

    authenticate to spotify

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 const tmi = require('tmi.js');
 const express = require('express');
+const spotifyRouter = require('./routers/spotify');
 const { parse } = require('path');
 const { createLogger } = require('bunyan');
-const login = require('./users/login');
-const logout = require('./users/logout');
 
 const OAUTH_TOKEN = process.env.OAUTH_TOKEN;
 const STREAMER_CHANNEL = process.env.STREAMER_CHANNEL;
@@ -11,21 +10,21 @@ const HELP_COMMAND = '!help';
 const SONG_REQUEST_COMMAND = '!song';
 const PORT = process.env.PORT || 3002;
 
-const PLAYLIST_SCOPES = 'playlist-modify-public playlist-modify-private';
-
 const logger = createLogger({
   name: parse(__filename).name,
   level: process.env.LOG_LEVEL || 'info',
 });
 
 const app = express();
+
+app.use(express.json());
+app.use(spotifyRouter);
 app.listen(PORT, () => {
   logger.info({
     message: `listening on ${PORT}`,
   });
 });
 
-let token;
 const client = new tmi.Client({
   options: {
     debug: true,
@@ -48,22 +47,6 @@ client.on('connected', async () => {
   logger.info({
     message: 'connected to twitch',
   });
-
-  try {
-    // TODO: connect to spotify?
-    token = await login();
-    logger.info({
-      token,
-      message: 'connected to spotify',
-    });
-    return token;
-  } catch (err) {
-    logger.info({
-      err,
-      message: 'failed to connect to spotify',
-    });
-    throw err;
-  }
 });
 
 client.on('message', async (channel, tags, message, self) => {
@@ -74,7 +57,7 @@ client.on('message', async (channel, tags, message, self) => {
     try {
       return await client.say(
         channel,
-        `@${tags.username}, use the command !shots to pour ${STREAMER_CHANNEL} a shot`
+        `@${tags.username}, use the command !song {your song here} to add a song to the playlist`
       );
     } catch (err) {
       await client.say(channel, `@${tags.username}, failed to provide help :(`);
@@ -103,21 +86,5 @@ client.on('message', async (channel, tags, message, self) => {
       });
       return err;
     }
-  }
-});
-
-client.on('disconnected', async () => {
-  logger.info({
-    message: 'disconnected from twitch',
-  });
-
-  try {
-    return await logout(token);
-  } catch (err) {
-    logger.info({
-      err,
-      message: 'failed to logout of shot bot',
-    });
-    throw err;
   }
 });
---
+++ src/routers/spotify.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const passport = require('passport');
+const { parse } = require('path');
+const { createLogger } = require('bunyan');
+const SpotifyStrategy = require('passport-spotify').Strategy;
+
+const logger = createLogger({
+  name: parse(__filename).name,
+  level: process.env.LOG_LEVEL || 'info',
+});
+
+const PORT = process.env.PORT || 3002;
+const PLAYLIST_SCOPES = [
+  'playlist-modify-public',
+  'playlist-modify-private',
+  'user-read-email',
+  'user-read-private',
+];
+
+const router = express.Router();
+
+passport.use(
+  new SpotifyStrategy(
+    {
+      clientID: 'clientid',
+      clientSecret: 'clientsecret',
+      callbackURL: `http://localhost:${PORT}/callback`,
+    },
+    (accessToken, refreshToken, expires_in, profile, done) => {
+      logger.info({
+        accessToken,
+        refreshToken,
+        expires_in,
+        profile,
+        message: 'successfully logged into spotify',
+      });
+      return done();
+    }
+  )
+);
+
+router.get(
+  '/auth/spotify',
+  passport.authenticate(
+    'spotify',
+    {
+      scope: PLAYLIST_SCOPES,
+      showDialog: true,
+    },
+    (req, res) => {
+      logger.info({
+        req,
+        res,
+        message: 'authenticating to spotify',
+      });
+    }
+  )
+);
+
+router.get(
+  '/callback',
+  passport.authenticate('spotify', { failureRedirect: '/login' }, () => {
+    logger.info({
+      message: 'successful authentication, redirecting home',
+    });
+  })
+);
+
+module.exports = router;
