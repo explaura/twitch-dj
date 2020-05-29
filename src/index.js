37a1b9e44bbf5f753b1e49436104d753496dad74
Laura <laurakbrandon@gmail.com>
2020-05-29 22:13:11 +1000

    add playlist command

--- src/index.js
+++ src/index.js
@@ -7,9 +7,14 @@ const { addSongToPlaylist, searchSong } = require('./services/spotify');
 
 const OAUTH_TOKEN = process.env.OAUTH_TOKEN;
 const STREAMER_CHANNEL = process.env.STREAMER_CHANNEL;
+const PORT = process.env.PORT || 3002;
+const SPOTIFY_PLAYLIST =
+  'https://open.spotify.com/playlist/7nyIsdoLSm4gnXBFo7hNZC?si=vs9KYML6SN6eOdSaHmRFnA';
+
+// Twitch commands
 const HELP_COMMAND = '!help';
 const SONG_REQUEST_COMMAND = '!song';
-const PORT = process.env.PORT || 3002;
+const PLAYLIST_COMMAND = '!playlist';
 
 const logger = createLogger({
   name: parse(__filename).name,
@@ -58,10 +63,10 @@ client.on('message', async (channel, tags, message, self) => {
     try {
       return await client.say(
         channel,
-        `@${tags.username}, use the command !song {your song here} to add a song to the playlist`
+        `@${tags.username} use the command !song {your song here} to add a song to the playlist`
       );
     } catch (err) {
-      await client.say(channel, `@${tags.username}, failed to provide help :(`);
+      await client.say(channel, `@${tags.username} failed to provide help :(`);
       logger.error({
         err,
         message: `failed to handle ${HELP_COMMAND} command`,
@@ -87,11 +92,25 @@ client.on('message', async (channel, tags, message, self) => {
     } catch (err) {
       await client.say(
         channel,
-        `@${tags.username}, failed to add your song to the playlist :(`
+        `@${tags.username} failed to add your song to the playlist :(`
       );
       logger.error({
         err,
         message: `failed to handle ${SONG_REQUEST_COMMAND} command`,
+      });
+      return err;
+    }
+  } else if (message.toLowerCase() === PLAYLIST_COMMAND) {
+    try {
+      return await client.say(
+        channel,
+        `@${tags.username} access the playlist here: ${SPOTIFY_PLAYLIST}`
+      );
+    } catch (err) {
+      await client.say(channel, `@${tags.username} failed to get the playlist`);
+      logger.error({
+        err,
+        message: `failed to handle ${SPOTIFY_PLAYLIST} command`,
       });
       return err;
     }
